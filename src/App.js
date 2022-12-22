e4398febccf82bedc3220d36f94e8ad8734998ea
Alp Olcay Mis <71964088+alpolcaymis@users.noreply.github.com>
2022-12-22 18:39:09 +0300

    Food Order Project
    To use and practice what I learned so far.
    useState, useReducer, useContext
    Array.filter(), Array.map()

--- src/App.js
+++ src/App.js
@@ -1,47 +1,29 @@
-import React, { useState } from "react";
-import Expenses from "./components/Expenses/Expenses";
-import "./App.css";
-import NewExpense from "./components/NewExpense/NewExpense";
-import ExpensesFilter from "./components/Expenses/ExpensesFilter";
+import { useState } from "react";
+import Meals from "./components/Meals/Meals";
+import Header from "./components/Layout/Header";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
 
-const App = () => {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
 
-  const addExpenseHandler = (expense) => {
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
-    });
+  const hideCartHandler = () => {
+    setCartIsShown(false);
   };
 
   return (
-    <div className="App-header">
-      <NewExpense onAddExpense={addExpenseHandler} />
-
-      <Expenses items={expenses} />
-    </div>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler}></Header>;
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
-};
+}
 
 export default App;
